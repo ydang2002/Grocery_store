753e0e72aa82aae796e783458a8918277d85fc8a
LAPTOP-38ED4DO3\ASUS <nhuy.danghuynh2002@gmail.com>
2023-03-05 15:56:27 +0700

    Thiết kế giao diện login và xử lý click sang màn hình login, signUp

--- app/src/main/java/com/nhuy/grocerystore/LoginActivity.java
+++ app/src/main/java/com/nhuy/grocerystore/LoginActivity.java
@@ -2,13 +2,41 @@ package com.nhuy.grocerystore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button signIn;
+    EditText email, password;
+    TextView signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        signIn = findViewById(R.id.login_btn);
+        email = findViewById(R.id.email_login);
+        password = findViewById(R.id.password_login);
+        signUp = findViewById(R.id.sign_up);
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
     }
 }
--- app/src/main/java/com/nhuy/grocerystore/RegisterActivity.java
+++ app/src/main/java/com/nhuy/grocerystore/RegisterActivity.java
@@ -2,13 +2,42 @@ package com.nhuy.grocerystore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    Button signUp;
+    EditText name, email, password;
+    TextView signIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        signUp = findViewById(R.id.sign_up_btn);
+        name = findViewById(R.id.name_reg);
+        email = findViewById(R.id.email_reg);
+        password = findViewById(R.id.password_reg);
+        signIn = findViewById(R.id.sign_in);
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
