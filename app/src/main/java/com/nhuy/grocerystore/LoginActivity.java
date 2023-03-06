e51aabe467e73d73a5f349a44eae19a6c3a6bd2c
LAPTOP-38ED4DO3\ASUS <nhuy.danghuynh2002@gmail.com>
2023-03-06 22:00:11 +0700

    Splash Screen

--- app/src/main/java/com/nhuy/grocerystore/LoginActivity.java
+++ app/src/main/java/com/nhuy/grocerystore/LoginActivity.java
@@ -35,7 +35,7 @@ public class LoginActivity extends AppCompatActivity {
         signIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+
             }
         });
     }
