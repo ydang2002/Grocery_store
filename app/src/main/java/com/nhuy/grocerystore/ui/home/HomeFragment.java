fb264a9cd5f874b422f3722f2d7ee19f1410f719
LAPTOP-38ED4DO3\ASUS <nhuy.danghuynh2002@gmail.com>
2023-03-11 01:40:05 +0700

    image slider

--- app/src/main/java/com/nhuy/grocerystore/ui/home/HomeFragment.java
+++ app/src/main/java/com/nhuy/grocerystore/ui/home/HomeFragment.java
@@ -1,25 +1,74 @@
 package com.nhuy.grocerystore.ui.home;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.nhuy.grocerystore.R;
+import com.nhuy.grocerystore.adapter.PopularAdapter;
 import com.nhuy.grocerystore.databinding.FragmentHomeBinding;
+import com.nhuy.grocerystore.models.PopularModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    RecyclerView popularRec;
+    FirebaseFirestore db;
 
+    //popular items
+    List<PopularModel> popularModelList;
+    PopularAdapter popularAdapter;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        db = FirebaseFirestore.getInstance();
+
+        popularRec = root.findViewById(R.id.pop_rec);
+
+        //Popular items
+        popularRec.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL,false));
+        popularModelList = new ArrayList<>();
+        popularAdapter = new PopularAdapter(getActivity(), popularModelList);
+        popularRec.setAdapter(popularAdapter);
+
+        db.collection("PopularProducts")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                PopularModel popularModel = document.toObject(PopularModel.class);
+                                popularModelList.add(popularModel);
+                                popularAdapter.notifyDataSetChanged();
+                            }
+                        } else {
+                            Toast.makeText(getActivity(), "Error"+task.getException(), Toast.LENGTH_SHORT).show();
+                            Log.d(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
 
         return root;
     }
