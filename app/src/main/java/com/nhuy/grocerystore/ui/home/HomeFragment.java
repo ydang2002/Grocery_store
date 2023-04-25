65d1097c0a62b8bc2d67ece1f340be35258c6428
LAPTOP-38ED4DO3\ASUS <nhuy.danghuynh2002@gmail.com>
2023-04-25 21:21:52 +0700

    Tìm kiếm

--- app/src/main/java/com/nhuy/grocerystore/ui/home/HomeFragment.java
+++ app/src/main/java/com/nhuy/grocerystore/ui/home/HomeFragment.java
@@ -3,10 +3,13 @@ package com.nhuy.grocerystore.ui.home;
 import static android.content.ContentValues.TAG;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.ScrollView;
 import android.widget.TextView;
@@ -20,6 +23,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -27,10 +31,12 @@ import com.nhuy.grocerystore.R;
 import com.nhuy.grocerystore.adapter.HomeAdapter;
 import com.nhuy.grocerystore.adapter.PopularAdapter;
 import com.nhuy.grocerystore.adapter.RecommendedAdapter;
+import com.nhuy.grocerystore.adapter.ViewAllAdapter;
 import com.nhuy.grocerystore.databinding.FragmentHomeBinding;
 import com.nhuy.grocerystore.models.HomeCategory;
 import com.nhuy.grocerystore.models.PopularModel;
 import com.nhuy.grocerystore.models.RecommendedModel;
+import com.nhuy.grocerystore.models.ViewAllModel;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -54,6 +60,13 @@ public class HomeFragment extends Fragment {
     //Recommended
     List<RecommendedModel> recommendedModelList;
     RecommendedAdapter recommendedAdapter;
+
+    //Search view
+    EditText search_box;
+    private List<ViewAllModel> viewAllModelList;
+    private RecyclerView recyclerViewSearch;
+    private ViewAllAdapter viewAllAdapter;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -144,7 +157,59 @@ public class HomeFragment extends Fragment {
                     }
                 });
 
+        //Search View
+        recyclerViewSearch = root.findViewById(R.id.search_rec);
+        search_box = root.findViewById(R.id.search_box);
+        viewAllModelList = new ArrayList<>();
+        viewAllAdapter = new ViewAllAdapter(getContext(), viewAllModelList);
+        recyclerViewSearch.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerViewSearch.setAdapter(viewAllAdapter);
+        recyclerViewSearch.setHasFixedSize(true);
+        search_box.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (editable.toString().isEmpty()){
+                    viewAllModelList.clear();
+                    viewAllAdapter.notifyDataSetChanged();
+                }else {
+                    searchProduct(editable.toString());
+                }
+            }
+        });
+
         return root;
+    }
+
+    private void searchProduct(String type) {
+
+        if (!type.isEmpty()){
+            db.collection("AllProducts").whereEqualTo("type", type).get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful() && task.getResult() != null){
+                                viewAllModelList.clear();
+                                viewAllAdapter.notifyDataSetChanged();
+
+                                for (DocumentSnapshot doc : task.getResult().getDocuments()){
+                                    ViewAllModel viewAllModel = doc.toObject(ViewAllModel.class);
+                                    viewAllModelList.add(viewAllModel);
+                                    viewAllAdapter.notifyDataSetChanged();
+                                }
+                            }
+                        }
+                    });
+        }
     }
 
 
